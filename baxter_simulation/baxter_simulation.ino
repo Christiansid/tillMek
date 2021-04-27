e448a2a011dc44fd7ab01c99fd93f3f31d7b04f3
Berntzone <67507274+Berntzone@users.noreply.github.com>
2021-04-27 16:34:15 +0200

    Updaterad arduinokod

--- baxter_simulation/baxter_simulation.ino
+++ baxter_simulation/baxter_simulation.ino
@@ -1,28 +1,22 @@
-const byte buttonPin = 2;  // Pin to control the Simulation
 const byte stopPin = 3;    // Input pin detecting when system has registered a stop
 const byte floatPin = 4;   // Output pin sending simulated float switch signals to circuit
 const byte led_arduino = 13;
 
 
-#define SEND_DELAY 1000   // Set time between messages to Serial port 
-unsigned long last_send = 0;      // Keeps track of how often to send serial data
+unsigned long last_stop = 0;                // Keeps track of how often to stop the float switch simulation
 
 volatile bool circuitState = false;         // For sending circuit state to serial
 volatile bool simulateFloat = true;         // For sending simulation state to serial
 
 // PUMPSIMULATION                             // 15ml/m - 350ml/m
-int flowrate = 15;                            // Slagvolym 0.7ml/slag
+int flowrate = 20;                            // Slagvolym 0.7ml/slag
 int pump_delay = 1000/(flowrate/0.7)*60;      // 0.33 - 8.333 slag/s
-
-bool handshake = false;
-
 
 
 void setup() {
   pinMode(floatPin, OUTPUT);
   pinMode(led_arduino, OUTPUT);
   pinMode(stopPin, INPUT);
-  pinMode(buttonPin, INPUT);
   Serial.begin(9600);
 
   // SIMPLE HANDSHAKE
@@ -35,7 +29,6 @@ void setup() {
     }
   }
   
-  attachInterrupt(digitalPinToInterrupt(buttonPin), simulateStop, RISING);
   attachInterrupt(digitalPinToInterrupt(stopPin), circuitStop, CHANGE);
   sei();
 }
@@ -48,22 +41,28 @@ void loop() {
   // SIMULATE FLOAT SWITCHES
   if (simulateFloat){
     digitalWrite(floatPin, HIGH);
+    digitalWrite(led_arduino, HIGH);
     delay(pump_delay/2);
-    digitalWrite(floatPin, LOW); 
+    digitalWrite(floatPin, LOW);
+    digitalWrite(led_arduino, LOW); 
     delay(pump_delay/2);
 
     sendInfo();
   } 
   else 
   {
-   delay(1000); 
+   delay(pump_delay); 
    sendInfo();
   }
 
+
+  int curr_time = millis();
+  if (curr_time - last_stop > 10000){
+    simulateFloat =! simulateFloat;
+    last_stop = curr_time;
+  }
  
 }
-
-
 
 
 // CONTROLLED BY MANUAL BUTTON (with debounce)
@@ -80,16 +79,17 @@ void simulateStop() {
 
 }
 
+
 // TRIGGERED WHEN CIRCUIT RECOGNISES PUMP FAILURE
 void circuitStop() {               
   circuitState = digitalRead(stopPin);
   sendInfo();
 
-  if (circuitState) {
-    digitalWrite(led_arduino, HIGH);
-  }
-  
+  simulateFloat = true;
+  circuitState = false;
+
 }
+
 
 // SEND DATA
 void sendInfo() {
@@ -99,24 +99,4 @@ void sendInfo() {
   Serial.print(simulateFloat);
   Serial.print(circuitState);
 
-}
-
-// RECIEVE DATA
-void RecieveData() {
-  if (Serial.available() > 0)
-    {
-        int rate = Serial.read();
-        Serial.print(rate);
-        Serial.print('\n');
-        
-        flowrate = rate;
-        delay(1000);
-        
-    }
-    else
-    {
-      Serial.print(10); // Failed.
-    }
-
-  
 }
